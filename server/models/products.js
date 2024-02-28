ff73d10e2a59530313386140faba6302d4d9cb39
Zazixxs <125758932+Zazixxs@users.noreply.github.com>
2024-02-28 14:53:32 +0100

    update

---
+++ server/models/products.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("products",{
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        createdAt:{
+            type:DataTypes.DATE,
+            allowNull:false,
+        },
+        updatedAt:{
+            type:DataTypes.DATE,
+            allowNull:false
+        },
+        titel:{
+            type:DataTypes.STRING(100),
+            allowNull:false
+        },
+        description:{
+            type:DataTypes.TEXT,
+            allowNull:false
+        },
+        price:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        imageUrl:{
+            type : DataTypes.STRING(255)
+        }
+    }, {underscored : true })
+}
